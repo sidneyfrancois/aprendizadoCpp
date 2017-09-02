c198f1000c8f138079ad28ee0578500e7c1c68e6
sidneyfrancois <sidney.fvm@hotmail.com>
2017-09-02 18:15:06 -0300

    diretório inserido

---
+++ exemplos/ex3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+	double x;
+
+	x = sqrt(6.25);
+
+	cout << x << endl;
+
+	return 0;
+}
---
+++ exemplos/ex5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int stonetolb(int);
+
+int main()
+{
+	int stone;
+
+	cout << "Enter the weight in the stone: ";
+	cin >> stone;
+
+	int pounds = stonetolb(stone);
+
+	cout << stone << " stone = ";
+	cout << pounds << " pounds." << endl;
+
+	return 0;
+}
+
+int stonetolb(int sts){
+	return 14 * sts;
+}
