12497d07b734daa5f207699a488dfd2b56139ba3
sidneyfrancois <sidney.fvm@hotmail.com>
2017-09-17 15:06:05 -0300

    finalizando lista3

---
+++ pc2/lista3/ex6.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct pessoa Pessoa;
+
+struct pessoa{
+	char nome[50];
+	char dataNascimento[20];
+	char cpf[20];
+};
+
+void preenche_estrutura(Pessoa *p);
+void imprime_estrutura(Pessoa *p);
+
+int main()
+{
+	Pessoa *p1;
+	
+	p1 = (Pessoa*)malloc(sizeof(Pessoa));	
+
+	preenche_estrutura(p1);	
+	imprime_estrutura(p1);
+
+	free(p1);
+	return 0;
+}
+
+void preenche_estrutura(Pessoa *p){
+	
+	printf("Digite o nome da pessoa: ");
+	scanf("%s", (*p).nome);
+	
+	printf("Digite a data de nascimento da pessoa: ");
+	scanf("%s", (*p).dataNascimento);
+
+	printf("Digite o cpf da pessoa: ");
+	scanf("%s", (*p).cpf);
+ 
+}
+
+void imprime_estrutura(Pessoa *p){
+	
+	printf("\nNome da pessoa: %s", (*p).nome);
+	printf("\nData de nascimento da pessoa: %s", (*p).dataNascimento);
+	printf("\nCPF da pessoa: %s\n", (*p).cpf);
+ 
+}
